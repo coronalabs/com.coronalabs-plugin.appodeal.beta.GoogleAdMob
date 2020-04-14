8d26a648d74bd0d740ed6d1f86a3f6471a171de5
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:57:04 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDGoogleAdMobAdapter', },
+		frameworks = { 'GoogleAppMeasurement', 'GoogleMobileAds', 'GoogleUtilities', 'nanopb', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
